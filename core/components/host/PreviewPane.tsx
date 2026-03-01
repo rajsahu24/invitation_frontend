60e56da3da18b5a06272ee35fc95712015f5cc7a
Raj Sahu <55652441+rajsahu-git@users.noreply.github.com>
2026-03-01 17:16:32 +0530

    addded config in next js

--- core/components/host/PreviewPane.tsx
+++ core/components/host/PreviewPane.tsx
@@ -25,7 +25,7 @@ const PreviewPane: React.FC<PreviewPaneProps> = ({ url, isLoading = false, realT
   const iframeRef = useRef<HTMLIFrameElement>(null);
   const [templateUrl, setTemplateUrl] = useState<string>('');
   const { selectedTemplate } = useHostStore();
-  const button_url =  `${process.env.NEXT_PUBLIC_TEMPLATE_APIGATEWAY_URL}/${public_id}`
+  const button_url =  `https://inviteera.com/${public_id}`
   console.log(selectedTemplate  )
   useEffect(() => {
     console.log('PreviewPane useEffect triggered:', { public_id, selectedTemplate, refreshKey });
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,32 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return {
+      afterFiles: [
+        {
+          source: "/@vite/:path*",
+          destination: `${process.env.NEXT_PUBLIC_TEMPLATE_APIGATEWAY_URL}/@vite/:path*`,
+        },
+        {
+          source: "/src/:path*",
+          destination: `${process.env.NEXT_PUBLIC_TEMPLATE_APIGATEWAY_URL}/src/:path*`,
+        },
+        {
+          source: "/node_modules/:path*",
+          destination: `${process.env.NEXT_PUBLIC_TEMPLATE_APIGATEWAY_URL}/node_modules/:path*`,
+        },
+        {
+          source: "/assets/:path*",
+          destination: `${process.env.NEXT_PUBLIC_TEMPLATE_APIGATEWAY_URL}/assets/:path*`,
+        },
+        {
+          source: "/:public_id",
+          destination: `${process.env.NEXT_PUBLIC_TEMPLATE_APIGATEWAY_URL}/:public_id`,
+        },
+      ],
+    };
+  },
 };
 
 export default nextConfig;
