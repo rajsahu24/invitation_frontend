6837cf056980ccd83d54b0e266c0e42a8f9d57f2
Raj Sahu <55652441+rajsahu-git@users.noreply.github.com>
2026-03-11 20:12:00 +0530

    bug fix

--- core/components/landingV2/Navigation.tsx
+++ core/components/landingV2/Navigation.tsx
@@ -45,7 +45,7 @@ export default function Navigation() {
 
   return (
     <nav
-      className={`sticky top-0 left-0 right-0 z-50 h-20 ${
+      className={`fixed top-0 left-0 right-0 z-50 transition-all duration-300 h-20  ${
         scrolled
           ? 'bg-[--color-bg-primary]/95 backdrop-blur-[12px] border-b border-[--color-border]'
           : 'bg-[--color-bg-primary]'
