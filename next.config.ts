01e2322aceb6021a5c2ddff29ca27030dafee6eb
Raj Sahu <55652441+rajsahu-git@users.noreply.github.com>
2026-03-18 14:01:05 +0530

    bug fix

--- next.config.ts
+++ next.config.ts
@@ -1,36 +1,34 @@
 import type { NextConfig } from "next";
 
+const TEMPLATE_URL = process.env.NEXT_PUBLIC_TEMPLATE_APIGATEWAY_URL;
+
 const nextConfig: NextConfig = {
   async rewrites() {
     return {
       afterFiles: [
         {
-          source: "/@vite/:path*",
-          destination: `${process.env.NEXT_PUBLIC_TEMPLATE_APIGATEWAY_URL}/@vite/:path*`,
-        },
-        {
-          source: "/src/:path*",
-          destination: `${process.env.NEXT_PUBLIC_TEMPLATE_APIGATEWAY_URL}/src/:path*`,
-        },
-        {
-          source: "/node_modules/:path*",
-          destination: `${process.env.NEXT_PUBLIC_TEMPLATE_APIGATEWAY_URL}/node_modules/:path*`,
+          source: "/:path*",
+          destination: `${TEMPLATE_URL}/:path*`,
         },
         {
           source: "/assets/:path*",
-          destination: `${process.env.NEXT_PUBLIC_TEMPLATE_APIGATEWAY_URL}/assets/:path*`,
+          destination: `${TEMPLATE_URL}/assets/:path*`,
         },
         {
-          source: "/:slug",
-          destination: `${process.env.NEXT_PUBLIC_TEMPLATE_APIGATEWAY_URL}/:slug`,
-        },
-        {
-          source: "/public/:public_id",
-          destination: `${process.env.NEXT_PUBLIC_TEMPLATE_APIGATEWAY_URL}/public/:public_id`,
+          source: "/_next/static/:path*",
+          destination: `${TEMPLATE_URL}/_next/static/:path*`,
         },
         {
           source: "/preview/:path*",
-          destination: `${process.env.NEXT_PUBLIC_TEMPLATE_APIGATEWAY_URL}/preview/:path*`,
+          destination: `${TEMPLATE_URL}/preview/:path*`,
+        },
+        {
+          source: "/public/:public_id",
+          destination: `${TEMPLATE_URL}/public/:public_id`,
+        },
+        {
+          source: "/invitation/:id",
+          destination: `${TEMPLATE_URL}/invitation/:id`,
         },
       ],
     };
